6c1124bcaff005c58020da0dd4891fb422b76c1c
Anthony Rurup <tcrurup@gmail.com>
2019-04-29 21:41:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Dog 
+  
+  def name=(given_name)
+    @this_dogs_name = given_name
+  end
+  
+  def name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
